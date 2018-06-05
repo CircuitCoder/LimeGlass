21eda9db5dffeea70524f3967fe2db5260c28ef1
Liu Xiaoyi <circuitcoder0@gmail.com>
2018-06-05 18:51:49 +0800

    Fixed: potential risk of losing assignment data

--- utils/db.mjs
+++ utils/db.mjs
@@ -2,7 +2,7 @@ export function bulkUpsert(Model, values) {
   return new Promise((resolve, reject) => {
     const bulk = Model.collection.initializeUnorderedBulkOp();
     for(const v of values)
-      bulk.find({ _id: v._id }).upsert().updateOne(v);
+      bulk.find({ _id: v._id }).upsert().updateOne({ $set: v });
     bulk.execute((err, res) => {
       if(err) return reject(err);
       else return resolve(res);
